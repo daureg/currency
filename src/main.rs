ee7ec10c1a4a6d067dd9a80dceb0d52746f4766e
Géraud Le Falher <daureg@gmail.com>
2015-09-12 18:40:11 +0200

    read URL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate hyper;
+
+use std::io::Read;
+use hyper::Client;
+
+fn read_url(client: Client, url: &str, response: &mut String) {
+    let mut res = client.get(url).send().unwrap();
+    res.read_to_string(response).unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let client = Client::new();
+    let mut body = String::new();
+    read_url(client, "http://www.floatrates.com/daily/eur.json", &mut body);
+    println!("{}", body);
 }
